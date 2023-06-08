9dbd88b0df74008ff281e5395d5fb33489affff1
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-06-08 15:25:30 +0200

    Send curl output til stdout

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ set -e
 
 quarto render index.qmd
 
-curl -X PUT -F index.html=@index.html \
+curl -o - -X PUT -F index.html=@index.html \
     "https://${ENV}/quarto/update/${QUARTO_ID}" \
     -H "Authorization:Bearer ${NADA_TOKEN}"
