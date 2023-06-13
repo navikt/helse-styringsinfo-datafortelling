0a66611626c9769207ea7d4ef0446e7d046cdf9d
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-06-13 09:53:09 +0200

    Kjør curl med verbose flag

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ set -e
 
 quarto render index.qmd
 
-curl -o - -X PUT -F index.html=@index.html \
+curl -v -o - -X PUT -F index.html=@index.html \
     "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
     -H "Authorization:Bearer ${NADA_TOKEN}"
