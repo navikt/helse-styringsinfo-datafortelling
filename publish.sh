c78dae9b4e203e501d94a258c16114ac3f3ae550
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-06-13 09:39:45 +0200

    Legg til timestamp og bruk riktig url

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ set -e
 quarto render index.qmd
 
 curl -o - -X PUT -F index.html=@index.html \
-    "https://${ENV}/quarto/update/${QUARTO_ID}" \
+    "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
     -H "Authorization:Bearer ${NADA_TOKEN}"
