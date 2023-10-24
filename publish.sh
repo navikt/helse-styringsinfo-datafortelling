7566dd7e16ce7edb539cffed0027c3ff31acc820
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-10-24 08:57:01 +0200

    Fjerner verbose output fra curl

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ set -e
 
 quarto render index.qmd
 
-curl -v -o - -X PUT -F index.html=@index.html \
+curl -o - -X PUT -F index.html=@index.html \
     "https://${NADA_URL}/quarto/update/${QUARTO_ID}" \
     -H "Authorization:Bearer ${NADA_TOKEN}"
