426857771c1dbc0b889e773fb5d2f175a20a6859
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-01-23 11:08:32 +0100

    Ny spørring for glidende gjennomsnitt fra 1.april
    Forrige forsøk brukte feil spørring.

--- figurer/fig_antall_med_glidende_gjennomsnitt.py
+++ figurer/fig_antall_med_glidende_gjennomsnitt.py
@@ -7,38 +7,42 @@ def antall_med_glidende_gjennomsnitt(
 ):
     # SQL query to count number of rows per day and calculate moving averages
     QUERY = f"""
-    SELECT
-    date,
-    count,
-    AVG(count) OVER (
-        ORDER BY date
-        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-    ) AS moving_7_day_avg,
-    AVG(count) OVER (
-        ORDER BY date
-        ROWS BETWEEN 27 PRECEDING AND CURRENT ROW
-    ) AS moving_28_day_avg
-    FROM (
-        SELECT date_range.date AS date, IFNULL(original_table.count, 0) AS count
-            FROM (
-                SELECT *
-                FROM UNNEST(GENERATE_DATE_ARRAY('2023-03-01', CURRENT_DATE())) AS date
-            ) AS date_range
-        LEFT JOIN (
+    WITH DatoListe AS (
+        SELECT DATE
+        FROM UNNEST(GENERATE_DATE_ARRAY('2023-03-01', CURRENT_DATE())) AS DATE
+    ),
+    DataPerDag AS (
         SELECT
             DATE({timestamp_felt}) AS date,
             COUNT(*) AS count
-        FROM
-            `{tabell}`
+        FROM `{tabell}`
         {predikat}
-        GROUP BY
-            DATE({timestamp_felt})
-            ) AS original_table
-        ON date_range.date = original_table.date
+        GROUP BY DATE({timestamp_felt})
+    ),
+    DatoerMedData AS (
+        SELECT
+            DatoListe.date AS date,
+            IFNULL(DataPerDag.count, 0) AS count
+        FROM DatoListe
+        LEFT JOIN DataPerDag ON DatoListe.date = DataPerDag.date
+    ),
+    KalkulertGjennomsnitt AS (
+        SELECT
+            date,
+            count,
+            AVG(count) OVER (
+                ORDER BY date
+                ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+            ) AS moving_7_day_avg,
+            AVG(count) OVER (
+                ORDER BY date
+                ROWS BETWEEN 27 PRECEDING AND CURRENT ROW
+            ) AS moving_28_day_avg
+        FROM DatoerMedData
     )
-    WHERE date >= '2023-03-01'
-    ORDER BY
-    date
+    SELECT * FROM KalkulertGjennomsnitt
+    WHERE date >= '2023-04-01'
+    ORDER BY date
     """
 
     query_job = client.query(QUERY)
